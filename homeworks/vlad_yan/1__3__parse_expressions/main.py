9f260348ee1464579e2edcab38164fd24b992fd0
kirmalyshev <kirill.malyshev@fogstream.ru>
2016-10-15 15:15:26 +1000

    Замечания по 3му заданию

--- homeworks/vlad_yan/1__3__parse_expressions/main.py
+++ homeworks/vlad_yan/1__3__parse_expressions/main.py
@@ -1,12 +1,22 @@
 import re
 from optparse import OptionParser
 
+"""
+PEP8
+Защита от дурака
+синтаксис в test.py
+
+Посмотри на результаты анализатора кода в pylint_result, исправь
+Добавь вычисление строк типа a + (b *a) - b или a-b+a*a
+"""
+
+
 floatnumber = '\d+(?:\.\d+)?'
 myregex = '(?P<operand1>{number})\s*(?P<operation>[\+\-\*/])\s*(?P<operand2>{number})'.format(number=floatnumber)
-myre = re.compile(myregex)
+myre = re.compile(myregex)  # Неудачное название переменной myre. Здесь и ниже
 
 
-def find_expressions(myre_, string):
+def find_expressions(myre_, string):  # Нижние подчеркивания нужны если работаем с private атрибутами класса.
     expressions = myre_.findall(string)
 
     return expressions
